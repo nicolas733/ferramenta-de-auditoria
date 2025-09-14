c3cce2b088768f5707ee9b50c29b01d3dad43dca
Nicolas H. Tancon <nicolastancon@gmail.com>
2025-09-14 14:57:40 -0300

    feat: create module with Spring Boot, maven and some dependencies

---
+++ Ferramenta Auditoria/src/main/java/br/com/ferramentaauditoria/FerramentaAuditoriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferramentaauditoria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerramentaAuditoriaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerramentaAuditoriaApplication.class, args);
+    }
+
+}
---
+++ Ferramenta Auditoria/src/test/java/br/com/ferramentaauditoria/FerramentaAuditoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferramentaauditoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerramentaAuditoriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
